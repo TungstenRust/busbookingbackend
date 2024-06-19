ca2072fc83bcb1f7feed778450c136651fadb89b
Shahin Safaroff <tungsten.rust@gmail.com>
2024-06-19 17:10:02 +0400

    first commit

---
+++ src/main/java/com/example/busbookingbackend/BusbookingbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.busbookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusbookingbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusbookingbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/busbookingbackend/BusbookingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busbookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusbookingbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
