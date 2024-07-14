377989637365551973f1955695a3bf338531c247
Shahin Safaroff <tungsten.rust@gmail.com>
2024-07-14 19:39:22 +0400

    JWT is added to dependencies4

---
+++ src/main/java/com/example/busbookingbackend/security/JwtUtil.java
@@ -0,0 +1,10 @@
+package com.example.busbookingbackend.security;
+
+import org.apache.catalina.User;
+
+public class JwtUtil {
+    private static final String SECRET_KEY = "secret_key";
+    public static String generateToken(String userName, Long roleId) {
+
+    }
+}
